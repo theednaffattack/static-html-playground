0edbcdf4dc98bf20b8aa4ee4af6f86bfc0d43e2a
Eddie Naff <eddie@eddienaff.dev>
2020-10-05 12:59:21 -0700

    Extract network ip address discovery function

--- src/server/network-ip-address.ts
+++ src/server/network-ip-address.ts
@@ -1,16 +1,7 @@
-import express from "express";
 import { networkInterfaces } from "os";
-import colors from "colors";
-import path from "path";
-import { renderFile } from "ejs";
-
-import { netIp } from "./network-ip-address";
-
-const port = 8080;
-const address = colors.green(`http://${netIp().en0}:${port}!`);
 
 // from: https://stackoverflow.com/a/8440736/9448010
-function netIp() {
+export function netIp() {
   const nets = networkInterfaces();
   const results = Object.create(null); // or just '{}', an empty object
   for (const name of Object.keys(nets)) {
@@ -31,22 +22,3 @@ function netIp() {
   }
   return results;
 }
-
-const app = express();
-
-const publicPath = path.join(__dirname, "../public");
-const viewsPath = path.join(__dirname, "../views");
-
-app.use(express.static(publicPath));
-
-app.engine("html", renderFile);
-app.set("view engine", "html");
-app.set("views", viewsPath);
-
-app.get("/", (req, res) => {
-  res.render("index.html");
-});
-
-app.listen(port, () => {
-  console.log(`\n\nYour app is listening at:\n` + address + "\n\n");
-});
